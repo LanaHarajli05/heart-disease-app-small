91be1062e9d2072e5ab69c209b299f4931855287
LanaHarajli05 <lanaharajli@gmail.com>
2025-06-14 03:02:55 +0300

    Update app_py_py.py

--- app_py_py.py
+++ app_py_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/10YqfYF9T41z0ITUArJn1ylwPKjrNJgaD
 """
 
-from google.colab import files
 uploaded = files.upload()
 
 import pandas as pd
